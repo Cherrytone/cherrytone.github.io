f36956cc43d79f04cebbf90387e496cd99f852f4
Cherrytone <38677783+Cherrytone@users.noreply.github.com>
2018-06-16 21:54:45 +0930

    data for storm center

---
+++ assignments/lesson8/script/stormcenter.js
@@ -0,0 +1,7 @@
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function thankYou(){
+    alert("We appreciate the time you took to submit this weather information, Thank you!");
+}
