b7fe992934d2f8a0b3de3b07f190be3b9349a7a5
Cherrytone <38677783+Cherrytone@users.noreply.github.com>
2018-06-23 11:50:52 +0930

    home page town info from JSON

--- assignments/lesson9/script/weather.js
+++ assignments/lesson9/script/weather.js
@@ -1,19 +1,45 @@
-var weatherRequest = new XMLHttpRequest();
-var currentWeather = "https://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=76a3770e7164ef89df9f9ede671deeeb&units=imperial";
-weatherRequest.open('GET', currentWeather, true);
+/*var weatherRequest = new XMLHttpRequest();
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=76a3770e7164ef89df9f9ede671deeeb&units=imperial";
+weatherRequest.open('GET', apiURL, true);
 weatherRequest.send();
 weatherRequest.onload =  function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
-    document.getElementById("current-temp").innerHTML=weatherData["main"]["temp"];
-}
+    document.getElementsByClassName("temp").innerHTML=weatherData["main"]["temp"];
+}*/
 var cityRequest = new XMLHttpRequest();
-var cityData = " https://byui-cit230.github.io/weather/data/towndata.json";
+var cityData = "https://byui-cit230.github.io/weather/data/towndata.json";
 cityRequest.open('GET', cityData, true);
-cityRequestRequest.send();
-cityRequestRequest.onload =  function () {
-    let weatherData = JSON.parse(cityRequestRequest.responseText);
-    console.log(weatherData);
-    document.getElementById("").innerHTML=[""][""];
+cityRequest.send();
+cityRequest.onload =  function() {
+    var townData = JSON.parse(cityRequest.responseText);
+//    console.log(townData);
+    var towns = townData["towns"];
+    var townHTML = "";
+    for (var i = 0; i < towns.length; i++) {
+        if (i == 0 || i == 1 || i == 3){
+        var name = towns[i]["name"];
+        var motto = towns[i]["motto"];
+        var year = towns[i]["yearFounded"];
+        var pop = towns[i]["currentPopulation"];
+        var annu_rain = towns[i]["averageRainfall"];
+        var eventHTML = '<ul>';
+        for (var j = 0; j < towns[i].events.length; j++) {
+            var event = towns[i].events[j];
+            eventHTML = eventHTML + `<li><span>${event}</span></li>`;
+        }
+        eventHTML = eventHTML + '</ul>';
+        townHTML = townHTML + `<h1 class="name">${name}</h1>
+                    <ul class="town_info">
+                        <li>Motto:<span class="motto">${motto}</span></li>
+                        <li>Year Founded:<span class="year">${year}</span></li>
+                        <li>Population:<span class="pop">${pop}</span></li>
+                        <li>Annual Rainfall:<span class="annu_rain">${annu_rain}</span></li>
+                        <li>Local Events:<span class="event">${eventHTML}</li>
+                    </ul>`;
+        }
+    }
+    document.getElementById("town_list").innerHTML= townHTML;
 }
-
+      
+    
--- assignments/lesson9/script/weatherapi.js
+++ assignments/lesson9/script/weatherapi.js
@@ -5,5 +5,5 @@ weatherRequest.send();
 weatherRequest.onload =  function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
-    document.getElementById("current-temp").innerHTML=weatherData["main"]["temp"];
+    document.getElementsById("current-temp").innerHTML=weatherData["main"]["temp"];
 }
