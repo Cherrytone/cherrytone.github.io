cf19065bf2240dc9ab3bfd5d5cd53477cb6c1667
Cherrytone <38677783+Cherrytone@users.noreply.github.com>
2018-06-23 16:33:40 +0930

    update needed

--- assignments/lesson9/script/weather.js
+++ assignments/lesson9/script/weather.js
@@ -1,19 +1,11 @@
-/*var weatherRequest = new XMLHttpRequest();
-var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=76a3770e7164ef89df9f9ede671deeeb&units=imperial";
-weatherRequest.open('GET', apiURL, true);
-weatherRequest.send();
-weatherRequest.onload =  function () {
-    let weatherData = JSON.parse(weatherRequest.responseText);
-    console.log(weatherData);
-    document.getElementsByClassName("temp").innerHTML=weatherData["main"]["temp"];
-}*/
+/*-------HOME PAGE CITY INFO-----*/
+function cityHome(){
 var cityRequest = new XMLHttpRequest();
 var cityData = "https://byui-cit230.github.io/weather/data/towndata.json";
 cityRequest.open('GET', cityData, true);
 cityRequest.send();
 cityRequest.onload =  function() {
     var townData = JSON.parse(cityRequest.responseText);
-//    console.log(townData);
     var towns = townData["towns"];
     var townHTML = "";
     for (var i = 0; i < towns.length; i++) {
@@ -29,17 +21,53 @@ cityRequest.onload =  function() {
             eventHTML = eventHTML + `<li><span>${event}</span></li>`;
         }
         eventHTML = eventHTML + '</ul>';
-        townHTML = townHTML + `<h1 class="name">${name}</h1>
-                    <ul class="town_info">
+        townHTML = townHTML + `<div class="city"><div class="city_info"><h1 class="name">${name}</h1>
+                    <ul class="list">
                         <li>Motto:<span class="motto">${motto}</span></li>
                         <li>Year Founded:<span class="year">${year}</span></li>
                         <li>Population:<span class="pop">${pop}</span></li>
                         <li>Annual Rainfall:<span class="annu_rain">${annu_rain}</span></li>
                         <li>Local Events:<span class="event">${eventHTML}</li>
-                    </ul>`;
+                    </ul></div></div>`;
         }
     }
     document.getElementById("town_list").innerHTML= townHTML;
+    }
 }
-      
+/*-------WEATHER SUMMARY-------*/
+function franklinWeather(){
+var weatherRequest = new XMLHttpRequest();
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=76a3770e7164ef89df9f9ede671deeeb&units=imperial";
+weatherRequest.open('GET', apiURL, true);
+weatherRequest.send();
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+    document.getElementById("current-temp").innerHTML = weatherData.main.temp;
+    document.getElementById("high").innerHTML = weatherData.main.temp_max;
+    document.getElementById("low").innerHTML = weatherData.main.temp_min;
+    document.getElementById("wind").innerHTML = weatherData.wind.speed;
+    }
+}
+/*------------WIND CHILL------------*/
+function windchill() {
+    var high = parseFloat(document.getElementById("high").innerHTML);
+    var low = parseFloat(document.getElementById("low").innerHTML);
+    var s = parseFloat(document.getElementById("wind").innerHTML);
+    var windchill = Math.round(computewindchill(high, low, s)); 
+
+document.getElementById("windchill").innerHTML = windchill;
+}
+function computewindchill(high, low, s){
+    var t = (high + low)/2;
+    var windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, .016) + 0.4275 * t * Math.pow(s, .016);
+    return windchill; 
+}
+/*-------FRANKLIN PAGE------*/
+function franklin(){
+    windchill();
+    franklinWeather();
+}
+
+
     
--- assignments/lesson9/script/weatherapi.js
+++ assignments/lesson9/script/weatherapi.js
@@ -4,6 +4,5 @@ weatherRequest.open('GET', apiURL, true);
 weatherRequest.send();
 weatherRequest.onload =  function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
-    console.log(weatherData);
-    document.getElementsById("current-temp").innerHTML=weatherData["main"]["temp"];
+    document.getElementById("current-temp").innerHTML = weatherData.main.temp;
 }
