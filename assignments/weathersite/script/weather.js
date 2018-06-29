e112784e47aed6e4d745ad1c73dac04ceb8c23fe
Cherrytone <38677783+Cherrytone@users.noreply.github.com>
2018-06-29 14:20:48 +0930

    adding images

--- assignments/weathersite/script/weather.js
+++ assignments/weathersite/script/weather.js
@@ -15,12 +15,18 @@ cityRequest.onload =  function() {
         var pop = towns[i]["currentPopulation"];
         var annu_rain = towns[i]["averageRainfall"];
         var eventHTML = '<ul>';
+        var images = '<img';
+        var imagesSrc = ["images/donuts.jpeg", "images/green.jpg", "images/butterfly.jpg"];
         for (var j = 0; j < towns[i].events.length; j++) {
             var event = towns[i].events[j];
             eventHTML = eventHTML + `<li><span>${event}</span></li>`;
         }
+        for (var p = 0; p < imagesSrc[p].length; p++) {
+            var imagesSrc = imagesSrc[p];
+            images = images  + `src="${imagesSrc}">`;
+        }
         eventHTML = eventHTML + '</ul>';
-        townHTML = townHTML + `<div class="city"><div class="city_info"><h1 class="name">${name}</h1>
+        townHTML = townHTML + `<div class="city"><div class="city_info"><h1 class="name">${name}</h1><div class="images">${images}</div>
                     <ul class="list">
                         <li>Motto:<span class="motto">${motto}</span></li>
                         <li>Year Founded:<span class="year">${year}</span></li>
