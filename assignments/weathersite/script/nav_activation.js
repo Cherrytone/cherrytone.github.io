ca608e3f84fb5c5bc33d9168304d0e6dea43d033
Cherrytone <38677783+Cherrytone@users.noreply.github.com>
2018-06-29 21:11:27 +0930

    nav hover and active corrections

--- assignments/weathersite/script/nav_activation.js
+++
@@ -1,9 +0,0 @@
-var nav = document.getElementById("nav_tabs");
-var tabs = nav.getElementsByClassName("tabs");
-    for (var i = 0; i < tabs.length; i++) {
-        tabs[i].addEventListener("click", function() {
-            var current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace(" active", "");
-            this.className += " active";
-              });
-      }
--- assignments/weathersite/script/weather.js
+++ assignments/weathersite/script/weather.js
@@ -7,35 +7,31 @@ cityRequest.onload =  function() {
     var townData = JSON.parse(cityRequest.responseText);
     var towns = townData["towns"];
     var townHTML = "";
+    var goodCities = ["franklin", "springfield", "greenville"];
     for (var i = 0; i < towns.length; i++) {
-        if (i == 0 || i == 1 || i == 3){
-        var name = towns[i]["name"];
-        var motto = towns[i]["motto"];
-        var year = towns[i]["yearFounded"];
-        var pop = towns[i]["currentPopulation"];
-        var annu_rain = towns[i]["averageRainfall"];
-        var eventHTML = '<ul>';
-        var images = '<img';
-        var imagesSrc = ["images/donuts.jpeg", "images/green.jpg", "images/butterfly.jpg"];
-        for (var j = 0; j < towns[i].events.length; j++) {
-            var event = towns[i].events[j];
-            eventHTML = eventHTML + `<li><span>${event}</span></li>`;
+        if (goodCities.indexOf(towns[i].name.toLowerCase()) > -1) {
+            var name = towns[i]["name"];
+            var motto = towns[i]["motto"];
+            var year = towns[i]["yearFounded"];
+            var pop = towns[i]["currentPopulation"];
+            var annu_rain = towns[i]["averageRainfall"];
+            var eventHTML = '<ul>';
+            for (var j = 0; j < towns[i].events.length; j++) {
+                var event = towns[i].events[j];
+                eventHTML = eventHTML + `<li><span>${event}</span></li>`;
+            }
+            var imgSrc = "images/" + name.toLowerCase() + "-events.jpeg";
+            eventHTML = eventHTML + '</ul>';
+            townHTML = townHTML + `<div class="city"><div class="city_info"><h1 class="name">${name}</h1><div class="images"><img src="${imgSrc}" /></div>
+                        <ul class="list">
+                            <li>Motto:<span class="motto">${motto}</span></li>
+                            <li>Year Founded:<span class="year">${year}</span></li>
+                            <li>Population:<span class="pop">${pop}</span></li>
+                            <li>Annual Rainfall:<span class="annu_rain">${annu_rain}</span></li>
+                            <li>Local Events:<span class="event">${eventHTML}</li>
+                        </ul></div></div>`;
+            }
         }
-        for (var p = 0; p < imagesSrc[p].length; p++) {
-            var imagesSrc = imagesSrc[p];
-            images = images  + `src="${imagesSrc}">`;
-        }
-        eventHTML = eventHTML + '</ul>';
-        townHTML = townHTML + `<div class="city"><div class="city_info"><h1 class="name">${name}</h1><div class="images">${images}</div>
-                    <ul class="list">
-                        <li>Motto:<span class="motto">${motto}</span></li>
-                        <li>Year Founded:<span class="year">${year}</span></li>
-                        <li>Population:<span class="pop">${pop}</span></li>
-                        <li>Annual Rainfall:<span class="annu_rain">${annu_rain}</span></li>
-                        <li>Local Events:<span class="event">${eventHTML}</li>
-                    </ul></div></div>`;
-        }
-    }
         document.getElementById("town_info").innerHTML= townHTML;
     }
 }
