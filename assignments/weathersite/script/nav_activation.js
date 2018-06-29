e41e041b4c877b1b684914dcfdf45dd60fbd3336
Cherrytone <38677783+Cherrytone@users.noreply.github.com>
2018-06-29 11:21:10 +0930

    Fix footer and article seciton layout

--- assignments/weathersite/script/nav_activation.js
+++ assignments/weathersite/script/nav_activation.js
@@ -1,11 +1,9 @@
-fuction navActivate(){
-    var header = document.getElementById("nav_tabs");
-    var btns = header.getElementsByClassName("tabs");
-    for (var i = 0; i < btns.length; i++) {
-          btns[i].addEventListener("click", function() {
+var nav = document.getElementById("nav_tabs");
+var tabs = nav.getElementsByClassName("tabs");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener("click", function() {
             var current = document.getElementsByClassName("active");
             current[0].className = current[0].className.replace(" active", "");
             this.className += " active";
-          };
+              });
       }
-  }
