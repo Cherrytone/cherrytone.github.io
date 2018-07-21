2124ca108d8752563ca00ae57ca6b728e9a31cad
Cherrytone <38677783+Cherrytone@users.noreply.github.com>
2018-07-21 10:38:36 +0930

    CSS work and html set up

---
+++ assignments/final_project/script/date.js
@@ -0,0 +1,23 @@
+function printDate() {
+  var d = new Date();
+  var month = d.getMonth();
+  var date = d.getDate();
+  var day = d.getDay();
+  document.getElementById("currentdate").innerHTML = "day + date+ "," + month";
+}
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd = '0'+dd
+} 
+
+if(mm<10) {
+    mm = '0'+mm
+} 
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
