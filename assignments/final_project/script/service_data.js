30111f3cade157a3ecdd045da850f4e83521df14
Cherrytone <38677783+Cherrytone@users.noreply.github.com>
2018-07-26 11:31:34 +0930

    trying to get json to work

--- assignments/final_project/script/service_data.js
+++ assignments/final_project/script/service_data.js
@@ -1,23 +1,12 @@
-function servicePage(){
-var serviceRequest = new XMLHttpRequest();
-var serviceData = JSON.parse(service.json);
-serviceRequest.open('GET', "script/service.json", true);
-serviceRequest.send();
-serviceRequest.onload =  function() {
-    var data = serviceData["service"];
-    var serviceHTML = "";
-    console.log = serviceData;
-            var type = type[i]["service"];
-            var price = price[i]["price"];
-            var eventHTML = '<ul>';
-            for (var j = 0; j < service[i].events.length; j++) {
-                var event = service[i].events[j];
+function serviceHome(){
+        var serviceDate = 
+            { "type": ["Tube/Tire Change","Wheel True","Replace/Adjust Brake Pads","Adjust Derailleur","Flat Attack added to tube","Install chain","Bleed Hydralic Disc Brake","Tape Drop bars","Front Shock Oil/Seal change","Professional Bike Fit","Bike Restoration"],
+            "price": ["$4.99-$6.99 (Tube/tire not included)","$12.99 - $29.99","$9.99-$14.99","$14.99","$6.99 per tube (Tube not included)","$14.99 ($9.99 with Tune-Up)","$14.99 ($9.99 with Tune-Up)","$14.99","$74.99","$149.99","Starting at $299.99(call for details)"]
             }
-            townHTML = townHTML + `<div class="city"><a href=${link}.html><div class="city_info"><p class="name">${name}</p><div class="images"><img src="${imgSrc}" /></div>
-                        <ul class="list">
-                            <li>Service:<span class="motto">${type}</span></li>
-                            <li>Price:<span class="year">${price}</span></li></ul>
-                    </div>`;
-            }
-        document.getElementById("service_info").innerHTML= townHTML;
+    var j = JSON.parse(serviceDate);
+    document.getElementById("service_info").innerHTML= j;
+
 }
+
+
+
