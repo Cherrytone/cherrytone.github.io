01e640ddd1da4a66833474a34b2f751d2b1a7757
Cherrytone <38677783+Cherrytone@users.noreply.github.com>
2018-07-26 12:44:31 +0930

    json data stuff

--- assignments/final_project/script/service_data.js
+++ assignments/final_project/script/service_data.js
@@ -5,18 +5,13 @@ function serviceHome(){
     serviceRequest.send();
     serviceRequest.onload =  function() {
         var serviceData = JSON.parse(serviceRequest.responseText);
-        var type = serviceData["type"];
-        var price = serviceData["price"];
-        var allHTML = "";
-        for (var t = 0; t < type[t]; t++) {
+        var service = serviceData["service"];
+        for (var t = 0; t < service[t]; t++) {
              type = type[t];
         }
-        for (var p = 0; p < price[p]; p++) {
-             price = price[p];
-            }
-        allHTML = `<li>${type}</li><li>${price}</li>`;
+        service = `<tr><th scope="col">${service}</th></tr>`;
 
-        document.getElementById("service_info").innerHTML= allHTML;
+        document.getElementById("service_info").innerHTML= service;
     }
 }
 
