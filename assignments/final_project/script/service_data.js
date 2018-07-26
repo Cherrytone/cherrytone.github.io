a0c29d1600303fdbf0b484c0072cb6d910f317e7
Cherrytone <38677783+Cherrytone@users.noreply.github.com>
2018-07-26 12:34:00 +0930

    json file data change

--- assignments/final_project/script/service_data.js
+++ assignments/final_project/script/service_data.js
@@ -1,12 +1,27 @@
 function serviceHome(){
-        var serviceDate = 
-            { "type": ["Tube/Tire Change","Wheel True","Replace/Adjust Brake Pads","Adjust Derailleur","Flat Attack added to tube","Install chain","Bleed Hydralic Disc Brake","Tape Drop bars","Front Shock Oil/Seal change","Professional Bike Fit","Bike Restoration"],
-            "price": ["$4.99-$6.99 (Tube/tire not included)","$12.99 - $29.99","$9.99-$14.99","$14.99","$6.99 per tube (Tube not included)","$14.99 ($9.99 with Tune-Up)","$14.99 ($9.99 with Tune-Up)","$14.99","$74.99","$149.99","Starting at $299.99(call for details)"]
+    var serviceRequest = new XMLHttpRequest();
+    var serviceData = "https://cherrytone.github.io/assignments/final_project/script/service.json";
+    serviceRequest.open('GET', serviceData, true);
+    serviceRequest.send();
+    serviceRequest.onload =  function() {
+        var serviceData = JSON.parse(serviceRequest.responseText);
+        var type = serviceData["type"];
+        var price = serviceData["price"];
+        var allHTML = "";
+        for (var t = 0; t < type[t]; t++) {
+             type = type[t];
+        }
+        for (var p = 0; p < price[p]; p++) {
+             price = price[p];
             }
-    var j = JSON.parse(serviceDate);
-    document.getElementById("service_info").innerHTML= j;
+        allHTML = `<li>${type}</li><li>${price}</li>`;
 
+        document.getElementById("service_info").innerHTML= allHTML;
+    }
 }
 
+
+
+    
 
 
